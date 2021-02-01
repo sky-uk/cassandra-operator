9344a7f8494a574d53208646f4d978fcd3eb688b
Chris Berry <bez625@gmail.com>
2021-02-01 10:23:11 +0000

    Update code-gen to use vendor path (#250)

--- cassandra-operator/hack/update-codegen.sh
+++ cassandra-operator/hack/update-codegen.sh
@@ -9,24 +9,24 @@ projectDir="${scriptDir}/.."
 codeGenDir="${projectDir}/vendor/k8s.io/code-generator"
 basePackage="github.com/sky-uk/cassandra-operator/cassandra-operator"
 groupVersion="cassandra:v1alpha1"
+VENDOR_DIR="${projectDir}/vendor"
+GEN_FILE_PATH="/pkg/apis/cassandra/v1alpha1/zz_generated.deepcopy.go"
+CLIENT_PATH="/pkg/client"
 
 # ensure code-generator is available in vendor directory
 # https://github.com/kubernetes/code-generator/issues/57#issuecomment-498310800
 cd ${projectDir}
 go mod vendor
 
-if [[ $projectDir != *$basePackage* ]]; then
-  echo "Please ensure cassandra-operator is cloned into github.com/sky-uk/cassandra-operator to ensure generated files are created in the right places"
-  exit 1
-fi
-
 chmod +x ${codeGenDir}/generate-groups.sh
 
-# Ensure that cassandra-operator is cloned under you GOPATH like so
-# $GOPATH/github.com/sky-uk/cassandra-operator
-# This is to ensure code-generator can be run from inside the vendor directory.
 ${codeGenDir}/generate-groups.sh all \
   ${basePackage}/pkg/client ${basePackage}/pkg/apis \
   ${groupVersion} \
   --go-header-file ${scriptDir}/empty-boilerplate.txt \
-  --output-base ${projectDir}/../../../..
+  --output-base ${VENDOR_DIR}
+
+
+cp "${VENDOR_DIR}/${basePackage}/${GEN_FILE_PATH}" "${projectDir}/${GEN_FILE_PATH}"
+cp -ur "${VENDOR_DIR}/${basePackage}/${CLIENT_PATH}" "${projectDir}/pkg/"
+
